008ce97787df43750171bd2963e7027192a316a7
Bobby Vasquez <shootbv@gmail.com>
2021-05-10 16:04:12 -0600

    Add code to Turn.js to pass initial instantiation tests in Turn-test.js

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,14 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
-const Box = require('../src/Turn.js');
+const Turn = require('../src/Turn.js');
 
 describe('Turn', () => {
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', () => {
+  it('should be an instance of Turn', () => {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });

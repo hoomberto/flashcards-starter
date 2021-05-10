dc3c8eb5e7cd81cffff75bec5502b7bbcb67c27f
Bobby Vasquez <shootbv@gmail.com>
2021-05-10 17:21:01 -0600

    Implement test and code for countCards method that counts the number of cards within the deck

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -26,4 +26,19 @@ describe('Deck', () => {
     expect(deck.cards).to.deep.equal([card1, card2, card3])
     expect(deck.cards[0]).to.be.an.instanceOf(Card);
   })
+
+  it('should be able to count the cards within the deck', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const card4 = new Card(7, 'What is the average airspeed velocity of an unladen European swallow?', ['18mph', '25mph', '37mph'], '25mph');
+    const deck = new Deck([card1, card2, card3]);
+    const deck2 = new Deck([card1, card2, card3, card4]);
+
+    let counted = deck.countCards()
+    let counted2 = deck2.countCards()
+
+    expect(counted).to.equal(3)
+    expect(counted2).to.equal(4)
+  });
 });

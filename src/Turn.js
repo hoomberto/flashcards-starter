ca5b9104293c72df66eddb6148431e9f71b608aa
Bobby Vasquez <shootbv@gmail.com>
2021-05-10 16:12:31 -0600

    Implement check to see if Turn can instantiate with a given guess and flash card, along with code to pass test

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,9 @@
-class Turn {
-  constructor() {
+const Card = require('../src/Card');
 
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess || 'No guess present';
+    this.card = card || 'No card present';
   }
 }
 
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Turn = require('../src/Turn.js');
+const Card = require('../src/Card');
 
 describe('Turn', () => {
   it('should be a function', () => {
@@ -11,5 +12,14 @@ describe('Turn', () => {
   it('should be an instance of Turn', () => {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it('should be able to have a guess and flashcard', () => {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+
+    expect(turn.card).to.be.an.instanceof(Card)
+    expect(turn.card.answers).to.include('pug');
+    expect(turn.guess).to.equal('pug');
   });
 });

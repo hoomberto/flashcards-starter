241da5761fa04355afebe79fb4f0f1f9b9d7dc84
Bobby Vasquez <shootbv@gmail.com>
2021-05-10 18:47:44 -0600

    Add initial instantation test for Game class

---
+++ test/Game-test.js
@@ -0,0 +1,19 @@
+const Turn = require('../src/Turn.js');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const Game = require('../src/Game');
+
+describe('Game', () => {
+
+  it('should be a function', () => {
+    const round = new Game();
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', () => {
+    const game = new Game();
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+})

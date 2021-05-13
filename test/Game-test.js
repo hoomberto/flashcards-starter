a36152af905b3d9d14ac816001a9abaed4eba7c0
Bobby Vasquez <shootbv@gmail.com>
2021-05-12 19:59:14 -0600

    Cut down code in Game-test.js by utilizing beforeEach

--- test/Game-test.js
+++ test/Game-test.js
@@ -7,25 +7,23 @@ const Round = require('../src/Round');
 const Game = require('../src/Game');
 
 describe('Game', () => {
-
+  let game;
+  beforeEach('Setup', () => {
+    game = new Game();
+  })
   it('should be a function', () => {
-    const round = new Game();
     expect(Game).to.be.a('function');
   });
 
   it('should be an instance of Game', () => {
-    const game = new Game();
     expect(game).to.be.an.instanceof(Game);
   });
 
   it('should create cards, put cards in deck, then create a new round using the deck upon game start', () => {
-
-    const game = new Game();
     game.start();
 
     expect(game.currentRound).to.be.an.instanceof(Round);
     expect(game.currentRound.deck).to.be.an.instanceof(Deck);
-    expect(game.currentRound.deck.cards[0]).to.be.an.instanceof(Card)
+    expect(game.currentRound.deck.cards[0]).to.be.an.instanceof(Card);
   });
-
-})
+});
